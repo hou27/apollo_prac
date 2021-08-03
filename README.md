b4f0c908e5b3872031874f894c2305b15dfb0073
hou27 <ataj125@gmail.com>
2021-08-03 11:08:31 +0000

    Router and reset CSS

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Apollo
 
 Built with React, Apollo and Graphql  
+
+[styled-components](https://styled-components.com/)
+[reactrouter](https://reactrouter.com/web/guides/quick-start)
+[reactapollo](https://www.apollographql.com/docs/react/)
+
+```
+npm i styled-components react-router-dom
+
+npm install @apollo/client graphql -- (apollo 3.4.3ver setting)
+```
+
+Reset CSS  
+```
+http://meyerweb.com/eric/tools/css/reset/ 
+v2.0 | 20110126
+License: none (public domain)
+```
--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,12 @@
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <Router>
+		<Route exact path='/' component={Home} />
+		<Route path='/:id' component={Detail} />
+	</Router>
 }
 
 export default App;
