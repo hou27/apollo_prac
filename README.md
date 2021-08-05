4b055f8d37c247fda843b85114fe2ae32e6235f9
hou27 <ataj125@gmail.com>
2021-08-05 10:22:40 +0000

    gql query

--- README.md
+++ README.md
@@ -18,3 +18,18 @@ http://meyerweb.com/eric/tools/css/reset/
 v2.0 | 20110126
 License: none (public domain)
 ```
+
+```
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+```
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,35 @@
-export default () => "Detail";
+//import React from "react";
+import { useParams } from "react-router-dom";
+import {
+  // ApolloClient,
+  // InMemoryCache,
+  // ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+const Detail = () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id }
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
+
+export default Detail;
