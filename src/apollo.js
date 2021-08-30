794508ceefc48c345326ea448ef9b20d48ad9620
hou27 <ataj125@gmail.com>
2021-08-30 11:14:42 +0000

    try writeQuery2

--- src/apollo.js
+++ src/apollo.js
@@ -2,7 +2,8 @@ import { InMemoryCache, ApolloClient, gql } from "@apollo/client";
 
 // const cache = new InMemoryCache();
 
-
+// https://velog.io/@yhe228/apollo-writeQuery-%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC-mutation%ED%9B%84-%EC%BA%90%EC%8B%9C-%EC%97%85%EB%8D%B0%EC%9D%B4%ED%8A%B8%ED%95%98%EA%B8%B0
+// 참고할 것.
 
 const client = new ApolloClient({
 	uri: "https://gqlserver.run.goorm.io",	// backend server (gql server playground)
@@ -21,36 +22,79 @@ const client = new ApolloClient({
 				});
     		},
 		  likeMovie: (_, { id }, { cache }) => {
-			likeFunc();
+			likeFunc(id, cache);
 		  }
 		}
 	}
 });
 
 function likeFunc (id, cache) {
-	cache.writeQuery({
-				query: gql`{
-					movie(id: $id) {
-					  id
-					  medium_cover_image
-					}
-				}`,
-				data: { // Contains the data to write
-					Movie: {
-						id: `Movie:${id}`,
-						medium_cover_image: "also can change this value"
-					}
-					//isLiked: true,isLiked @client
-					
-				}
-				// id: `Movie:${id}`,	// cache에서 형식 잘 살펴봐야함.
-				// data: {
-				// isLiked: true,
-				// medium_cover_image: "also can change this value"
-				// }
-			});
-			  console.log(id);
+	
+	const existingMovieData = cache?.readQuery({
+    	// 2. 현재 캐시에 저장되어있는 데이터를 가져온다.
+    	query: gql`{
+			movie(id: $id) {
+				id
+				medium_cover_image
+				isLiked @client
+			}
+		}`,
+        variables: { id: +id }
+    });
+	console.log(existingMovieData);
+	cache.writeQuery({ // 캐시 업데이트
+        query: gql`{
+			movie(id: $id) {
+				id
+				medium_cover_image
+				isLiked @client
+			}
+		}`,
+        variables: {
+          id: `Movie:${id}`, // GET_MESSAGES 쿼리는 매개변수로 roomID를 받는다 꼭 설정해주자! 꼭!!!
+        },
+        data: {
+			Movie: {
+				id: `Movie:${id}`,
+				medium_cover_image: "also can change this value",
+				isLiked: true
+			},
+        },
+    });
 }
+
+
+// const [sendMessage] = useMutation(SEND_MESSAGES, {
+//   update(cache, { data }) {
+//     // 1. 보낸 메시지를 가져온다
+//     const newMessage = data?.sendMessage?.sendMessage;
+
+//     const existingMessages = cache.readQuery({
+//       // 2. 현재 캐시에 저장되어있는 데이터를 가져온다.
+//       query: GET_MESSAGES,
+//       variables: {
+//         roomId: localRoomId, // GET_MESSAGES 쿼리는 매개변수로 roomID를 받는다 꼭 설정해주자! 
+//       },
+//     });
+
+//     if (newMessage && existingMessages) {
+//       cache.writeQuery({ // 캐시 업데이트!
+//         query: GET_MESSAGES,
+//         variables: {
+//           roomId: localRoomId, // GET_MESSAGES 쿼리는 매개변수로 roomID를 받는다 꼭 설정해주자! 꼭!!!
+//         },
+//         data: {
+//           getMessage: {
+//             ...existingMessages.getMessage,
+//             messages: [newMessage, ...existingMessages.getMessage.messages],
+//             // 3. 현재 캐시에 저장되어있는 messages에 새로운 message를 추가해준다.
+//           },
+//         },
+//       });
+//     }
+//   },
+// });
+
 // client.writeQuery({ query: IS_LOGGED_IN, data: { isLiked: true, medium_cover_image: "also can change this value"} });
 // client.writeQuery({
 //   query: gql`
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import {
+  InMemoryCache,
   useMutation,
   gql
 } from "@apollo/client";
@@ -40,10 +41,45 @@ const Poster = styled.div`
   background-position: center center;*/
 `;
 
+
 const Movie = ({ id, bg, isLiked }) => {
 	const [likeMovie] = useMutation(LIKE_MOVIE, {
 		variables: { id: +id }
 	});
+	
+	// const cache = new InMemoryCache();
+	
+	// const existingMovieData = cache.readQuery({
+	// // 2. 현재 캐시에 저장되어있는 데이터를 가져온다.
+	// query: gql`{
+	// 		movie(id: $id) {
+	// 			id
+	// 			medium_cover_image
+	// 			isLiked @client
+	// 		}
+	// 	}`,
+	// variables: { id: +id }
+	// });
+	
+	// cache.writeQuery({ // 캐시 업데이트!
+	// query: gql`{
+	// 		movie(id: $id) {
+	// 			id
+	// 			medium_cover_image
+	// 			isLiked @client
+	// 		}
+	// 	}`,
+	// variables: {
+	// id: `Movie:${id}`, // GET_MESSAGES 쿼리는 매개변수로 roomID를 받는다 꼭 설정해주자! 꼭!!!
+	// },
+	// data: {
+	// 		Movie: {
+	// 			id: `Movie:${id}`,
+	// 			medium_cover_image: "also can change this value",
+	// 			isLiked: true
+	// 		},
+	// },
+	// });
 	
 	return (
 		<Container>
